9923207ea19cb5eee660a6bded90f804360726ba
Phil Mlaker <philmlaker@gmail.com>
2017-02-05 11:36:19 -0600

    Got the routing to work

---
+++ app/routing/htmlRoutes.js
@@ -0,0 +1,45 @@
+// ===============================================================================
+// DEPENDENCIES
+// We need to include the path package to get the correct file path for our html
+// ===============================================================================
+var path = require('path');
+
+// // ===============================================================================
+// // ROUTING
+// // ===============================================================================
+
+
+module.exports = function (app) {
+	express = require('express');
+	app = express();
+
+
+
+    // HTML GET Requests
+    // Below code handles when users "visit" a page. 
+    // In each of the below cases the user is shown an HTML page of content
+    // ---------------------------------------------------------------------------
+
+    app.get("/:asdf?", function(req, res) {
+
+        var chosen = req.params.asdf;
+
+        if (chosen === "survey") {
+            res.sendFile(path.join(__dirname, "../public/survey.html"));
+        } else if (chosen === "home") {
+            res.sendFile(path.join(__dirname, "../public/home.html"));
+        } else {
+            res.sendFile(path.join(__dirname, "../public/home.html"));
+        }
+
+    });
+
+
+};
+
+
+
+// exports.test = function(){
+
+// 	console.log("this works")
+// }
